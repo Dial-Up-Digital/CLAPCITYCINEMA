2c80f6083b0a375bc1c0790e4c32dc9ebf236529
jbirori <jeffbirori@gmail.com>
2021-05-28 01:10:35 -0400

    Address eslint errors

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,6 @@
-export const mediaQueries = {
-    chatBreakpoint: '(max-width: 960px)',
-    mobile: '(max-width: 768px)',
-}
+const mediaQueries = {
+  chatBreakpoint: '(max-width: 960px)',
+  mobile: '(max-width: 768px)',
+};
+
+export default mediaQueries;
